c19044b44f5079c267d6369ae123ba7528f7e1a5
e.levin <e.levin@solarlab.ru>
2024-11-05 17:42:03 +0300

    Revert "Удалил лишние импорты"
    This reverts commit 992fc3ec64ff179a5e9c9a2d61fc5e483d11d52c.

--- src/main/java/com/sevsu/payment/exception/AmountCanNotBeLessZeroException.java
+++ src/main/java/com/sevsu/payment/exception/AmountCanNotBeLessZeroException.java
@@ -1,5 +1,7 @@
 package com.sevsu.payment.exception;
 
+import lombok.Data;
+
 public class AmountCanNotBeLessZeroException extends BadRequestException{
 
     public AmountCanNotBeLessZeroException(String message) {
