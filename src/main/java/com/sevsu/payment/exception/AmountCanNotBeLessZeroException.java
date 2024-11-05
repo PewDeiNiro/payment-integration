992fc3ec64ff179a5e9c9a2d61fc5e483d11d52c
e.levin <e.levin@solarlab.ru>
2024-11-05 17:41:36 +0300

    Удалил лишние импорты

--- src/main/java/com/sevsu/payment/exception/AmountCanNotBeLessZeroException.java
+++ src/main/java/com/sevsu/payment/exception/AmountCanNotBeLessZeroException.java
@@ -1,7 +1,5 @@
 package com.sevsu.payment.exception;
 
-import lombok.Data;
-
 public class AmountCanNotBeLessZeroException extends BadRequestException{
 
     public AmountCanNotBeLessZeroException(String message) {
