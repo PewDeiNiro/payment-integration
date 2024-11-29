197cccaadcdbb041065372c05c391f4a5b117316
e.levin <e.levin@solarlab.ru>
2024-11-29 20:07:10 +0300

    Починил мапперы

--- src/main/java/com/sevsu/payment/mapper/ExceptionResponseMapper.java
+++ src/main/java/com/sevsu/payment/mapper/ExceptionResponseMapper.java
@@ -5,8 +5,10 @@ import com.sevsu.payment.response.ExceptionResponse;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
+import org.mapstruct.ReportingPolicy;
 
-@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING,
+        unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface ExceptionResponseMapper {
 
     @Mapping(target = "code", expression = "java(httpException.getCode())")
--- src/main/java/com/sevsu/payment/mapper/InvoiceMapper.java
+++ src/main/java/com/sevsu/payment/mapper/InvoiceMapper.java
@@ -7,8 +7,10 @@ import com.sevsu.payment.response.InvoiceResponse;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
+import org.mapstruct.ReportingPolicy;
 
-@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING,
+        unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface InvoiceMapper {
 
     Invoice mapCreateInvoiceToInvoice(CreateInvoiceRequest request);
